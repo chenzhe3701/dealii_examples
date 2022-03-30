1d48ec7ff5ef827b057eca65a3fcb3b067830838
zhec-p14 <chenzhezju@gmail.com>
2022-03-30 17:24:34 -0400

    practice on step-2 on 1-D

--- myprog/main.cc
+++ myprog/main.cc
@@ -6,42 +6,46 @@
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_out.h>
 
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/dofs/dof_tools.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/dofs/dof_renumbering.h>
+
 using namespace dealii;
+
+void distribute_dofs(DoFHandler<1> &dof_handler){
+  const FE_Q<1> finite_element(1);
+  dof_handler.distribute_dofs(finite_element);
+  
+  DynamicSparsityPattern dynamic_sparsity_pattern(dof_handler.n_dofs(), dof_handler.n_dofs());
+  DoFTools::make_sparsity_pattern(dof_handler, dynamic_sparsity_pattern);
+
+  SparsityPattern sparsity_pattern;
+  sparsity_pattern.copy_from(dynamic_sparsity_pattern);
+
+  std::ofstream out("sparsity_pattern.svg");
+  sparsity_pattern.print_svg(out);
+}
 
 int main()
 {
-  std::cout << "Hello !" << std::endl;
+  Triangulation<1> triangulation;
+  GridGenerator::hyper_cube(triangulation, 10.0, 20.0);
+  triangulation.refine_global(2);
 
-  Triangulation<2> triangulation;
-  const Point<2> center(1,0);
-  const double inner_radius = 0.5, outer_radius = 1.0;
+  DoFHandler<1> dof_handler(triangulation);
 
-  GridGenerator::hyper_shell(triangulation, center, inner_radius, outer_radius, 10);
+  distribute_dofs(dof_handler);
 
-  for (unsigned int step = 0; step < 5; ++step)
-  {
-    for (auto &cell : triangulation.active_cell_iterators())
-    {
-      for (const auto v : cell->vertex_indices())
-      {
-        const double distance_from_center = center.distance(cell->vertex(v));
+  // plot triangulation mesh
+  std::ofstream out("gnuplot.gpl");
 
-        if ((cell->vertex(v)[1] > 0) && 
-          (std::fabs(distance_from_center - inner_radius) < 1e-6 * inner_radius))
-        {
-          cell->set_refine_flag();
-          break;
-        }
-      }     
-    }
+  out << "plot '-' using 1:2 with linespoints linewidth 2 pointsize 2 pointtype 7" << std::endl;
 
-    triangulation.execute_coarsening_and_refinement();
-  }
-  
-
-  std::ofstream out("grid-2.svg");
   GridOut grid_out;
-  grid_out.write_svg(triangulation, out);
-  std::cout << "Grid written to grid-2.svg" << std::endl;
+  grid_out.write_gnuplot(triangulation, out);
+  std::cout << "Grid written to grid plot" << std::endl;
 
 }
