7b0fa10d43b128e397f15b2aa734a09ce9dca433
zhec-p14 <chenzhezju@gmail.com>
2022-04-12 03:16:23 -0400

    plot both 1D and 2D mesh and dof

--- myprog/main.cc
+++ myprog/main.cc
@@ -16,50 +16,58 @@
 
 using namespace dealii;
 
-void distribute_dofs(DoFHandler<2> &dof_handler){
-  const FE_Q<2> finite_element(1);
-  dof_handler.distribute_dofs(finite_element);
-  
-  DynamicSparsityPattern dynamic_sparsity_pattern(dof_handler.n_dofs(), dof_handler.n_dofs());
-  DoFTools::make_sparsity_pattern(dof_handler, dynamic_sparsity_pattern);
+template <int dim>
+void gnuplot_mesh(Triangulation<dim> &triangulation, DoFHandler<dim> &dof_handler){
+  std::ofstream out("gnuplot.gpl");
 
-  SparsityPattern sparsity_pattern;
-  sparsity_pattern.copy_from(dynamic_sparsity_pattern);
+  out << "plot '-' using 1:2 with linespoints linewidth 2 pointsize 2 pointtype 7,"
+    << " '-' with labels point pt 2 offset 0,0 textcolor 'red' font ',24'"
+    << std::endl;
+  GridOut grid_out;
+  grid_out.write_gnuplot(triangulation, out);
 
-  std::ofstream out("sparsity_pattern.svg");
-  sparsity_pattern.print_svg(out);
+  std::map<types::global_dof_index, Point<dim>> support_points;
+  DoFTools::map_dofs_to_support_points(MappingQ1<dim>(), dof_handler, support_points);
+
+  out << "e" << std::endl;  // additional entry
+  if (dim==2) {
+    DoFTools::write_gnuplot_dof_support_point_info(out, support_points);
+  } 
+  else if (dim==1) {
+    for (const auto &[index, point] : support_points) {
+      out << point << " " << 1 << " " << '"' << index << '"' << std::endl;
+    }
+  }
+  out << "e" << std::endl;  // additional entry
+
+  std::cout << "Grid written to grid plot" << std::endl;
 }
 
 int main()
 {
-  Triangulation<2> triangulation;
+  const int dim {2};
+
+  Triangulation<dim> triangulation;
   GridGenerator::hyper_cube(triangulation, 10.0, 20.0);
   triangulation.refine_global(1);
 
-  DoFHandler<2> dof_handler(triangulation);
+  DoFHandler<dim> dof_handler(triangulation);
+  
+  const FE_Q<dim> finite_element(1);
+  dof_handler.distribute_dofs(finite_element);
+  
+  DoFRenumbering::Cuthill_McKee(dof_handler); // Renumber and plot sparsity pattern
 
-  distribute_dofs(dof_handler);
+  // plot sparsity pattern
+  DynamicSparsityPattern dsp(dof_handler.n_dofs(), dof_handler.n_dofs());
+  SparsityPattern sparsity_pattern;  
+  DoFTools::make_sparsity_pattern(dof_handler, dsp);
+  sparsity_pattern.copy_from(dsp);
+  std::ofstream out_sp("sparsity_pattern_1.svg");
+  sparsity_pattern.print_svg(out_sp);  
+  
 
   // plot triangulation mesh
-  std::ofstream out("gnuplot.gpl");
-
-  out << "plot '-' using 1:2 with linespoints linewidth 2 pointsize 2 pointtype 7,"
-      << " '-' with labels point pt 2 offset 0,0 textcolor 'red' font ',24'"
-      << std::endl;
-
-  GridOut grid_out;
-  grid_out.write_gnuplot(triangulation, out);
-
-  out << "e" << std::endl;
-  const int dim = 2;
-  if (dim==2)
-  {    
-    std::map<types::global_dof_index, Point<dim>> support_points;
-    DoFTools::map_dofs_to_support_points(MappingQ1<dim>(), dof_handler, support_points);
-    DoFTools::write_gnuplot_dof_support_point_info(out, support_points);
-    out << "e" << std::endl;
-  }
-
-  std::cout << "Grid written to grid plot" << std::endl;
+  gnuplot_mesh(triangulation, dof_handler);
 
 }
